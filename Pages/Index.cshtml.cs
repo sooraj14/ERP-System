593fdd544d3920611b390fe798ba774effeaf474
sooraj14 <chowkisooraj@gmail.com>
2024-11-29 11:34:52 +0530

    Revert "Reapply "2nd version""
    This reverts commit b6ff48d74bddeb8fcea560ccfa2c917f4a60bac7.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,3 @@
-using ERP_System.Data.context;
-using ERP_System.ModelClass;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,53 +7,14 @@ namespace ERP_System.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        private readonly ApplicationDbContext _context;
-        public IndexModel(ApplicationDbContext context)
+        public IndexModel(ILogger<IndexModel> logger)
         {
-            _context = context;
+            _logger = logger;
         }
-        [BindProperty]
-        public logincollge login {  get; set; }
-        
 
         public void OnGet()
         {
+
         }
-
-        public IActionResult OnPost()
-        {
-            var collegeadmin = _context.collegeadmins.FirstOrDefault(c=>c.college_email==login.email && c.college_pass==login.password);
-             HttpContext.Session.SetInt32("clge_admin", collegeadmin.college_id);
-            return RedirectToPage("Plans");
-        }
-        public IActionResult OnPostBasic()
-        {
-            int? id = HttpContext.Session.GetInt32("clge_admin");
-            var details = _context.collegeadmins.Where(cd => cd.college_id == (int)id).FirstOrDefault();
-            details.subscription = "Basic Plan";
-
-            _context.SaveChanges();
-            return RedirectToPage("Index");
-        }
-        public IActionResult OnPostStandard()
-        {
-            int? id = HttpContext.Session.GetInt32("clge_admin");
-            var details = _context.collegeadmins.Where(cd => cd.college_id == (int)id).FirstOrDefault();
-            details.subscription = "Standard Plan";
-
-            _context.SaveChanges();
-            return RedirectToPage("Index");
-        }
-
-        public IActionResult OnPostPremium()
-        {
-            int? id = HttpContext.Session.GetInt32("clge_admin");
-            var details = _context.collegeadmins.Where(cd => cd.college_id == (int)id).FirstOrDefault();
-            details.subscription = "Premium Plan";
-
-            _context.SaveChanges();
-            return RedirectToPage("Index");
-        }
-
     }
 }
