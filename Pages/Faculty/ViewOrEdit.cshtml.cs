03e639839b8324d4ad1664454d512a5ba3c2b2a0
Lakshmiprasad <lakshmiprasadk20@gmail.com>
2024-11-29 17:27:27 +0530

    marks update

--- Pages/Faculty/ViewOrEdit.cshtml.cs
+++ Pages/Faculty/ViewOrEdit.cshtml.cs
@@ -1,12 +1,28 @@
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ERP_System.Data.context;
+using ERP_System.Models;
+using System.Linq;
+using ERP_System.Data.Entity;
 
 namespace ERP_System.Pages.Faculty
 {
     public class ViewOrEditModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public ViewOrEditModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
         public void OnGet()
         {
+
         }
     }
+      
 }
